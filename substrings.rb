f6f797ed0a90d643f1eedc4e6f067802fe3e6be2
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-12-12 18:33:22 +0900

    Remove break from string.each loop

--- substrings.rb
+++ substrings.rb
@@ -1,21 +1,22 @@
+require 'pry-byebug'
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(input_string, dictionary)
 
-  string = input_string.split(" ")
+  string = input_string.downcase.split(" ")
 
   dictionary.reduce(Hash.new(0)) do |result, word|
     string.each do |match|
       if match.include?(word)
         result[word] += 1
-        break
       end
     end
     result
   end
 end
 
-p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
 
 
 # 'below'.include?('low') => true
