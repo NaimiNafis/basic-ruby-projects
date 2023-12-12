cef54e8d843a5870131e6e66df421f9d84c2c6fc
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-12-12 18:36:35 +0900

    Update variable names for readability

--- substrings.rb
+++ substrings.rb
@@ -1,22 +1,23 @@
 require 'pry-byebug'
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
 def substrings(input_string, dictionary)
+  input_words = input_string.downcase.split(" ")
 
-  string = input_string.downcase.split(" ")
-
-  dictionary.reduce(Hash.new(0)) do |result, word|
-    string.each do |match|
-      if match.include?(word)
-        result[word] += 1
+  word_count = dictionary.reduce(Hash.new(0)) do |count_hash, dictionary_word|
+    input_words.each do |input_word|
+      if input_word.include?(dictionary_word)
+        count_hash[dictionary_word] += 1
       end
     end
-    result
+    count_hash
   end
+  word_count
 end
 
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+
 p substrings("Howdy partner, sit down! How's it going?", dictionary)
+
 
 
 # 'below'.include?('low') => true
