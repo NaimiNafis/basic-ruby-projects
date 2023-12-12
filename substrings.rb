af500de340dfd9a171612aa4804503d3173fbcbe
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-12-12 14:30:17 +0900

    Add pseudocode

---
+++ substrings.rb
@@ -0,0 +1,22 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(strings, dictionary)
+
+end
+
+
+dictionary[0]
+
+# > substrings("Howdy partner, sit down! How's it going?", dictionary)
+# => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+
+=begin
+ FLOW
+ FOR EACH word in String, check if it same as Dic[]
+ REDUCE result_per_string, a
+  IF same THEN
+    result_per_string[a] += 1
+    REDUCE
+
+
+=end
