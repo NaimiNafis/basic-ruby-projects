5678dc432a4ff3dc0afb1f03d37948f134f1d8d2
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-12-12 16:29:20 +0900

    Update shifted_string to encrypted_string Split both upper lower case variable Merge new_index calculations into string each loop Add id space exist in string condition

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,49 +3,35 @@ require 'pry-byebug'
 def caesar_cipher(input_string = '', key = 0)
   string = input_string.split('')
 
-  binding.pry
+  alphabet_lower = 'abcdefghijklmnopqrstuvwxyz'.split("")
+  alphabet_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("")
 
-  alphabet_lower = ' abcdefghijklmnopqrstuvwxyz'
-  alphabet_upper = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  encrypted_string = []
 
-  alphabet_indices = []
-  shifted_string = []
-
-  string.each do |character|
-    if character == " "
-      alphabet_indices.push(0)
-    else
-      alphabet_lower.each_with_index do |alphabet_char, index|
-        if character == alphabet_char
-          alphabet_indices.push(index)
+  string.each do |char|
+    if alphabet_upper.include?(char)
+      alphabet_upper.each_with_index do |upper_char, index|
+        if char == upper_char
+          new_index = (index + key) % alphabet_upper.length
+          encrypted_string.push(alphabet_upper[new_index])
           break
         end
       end
-      alphabet_upper.each_with_index do |alphabet_char, index|
-        if character == alphabet_char
-          alphabet_indices.push(index)
+    elsif alphabet_lower.include?(char)
+      alphabet_lower.each_with_index do |lower_char, index|
+        if char == lower_char
+          new_index = (index + key) % alphabet_lower.length
+          encrypted_string.push(alphabet_lower[new_index])
           break
         end
       end
+    elsif char == " "
+      encrypted_string.push(" ")
     end
   end
 
-  binding.pry
 
-  shifted_indices = alphabet_indices.map do |index|
-    if index == 0
-      index
-    else
-      (index + key) % alphabet_lower.length
-  end
-
-  binding.pry
-
-  shifted_indices.each do |index|
-    shifted_string.push(alphabet_array[index])
-  end
-
-  p shifted_string.join
+  p encrypted_string.join
 end
 
 caesar_cipher('Zabc m', 2)
