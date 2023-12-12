7d5dffbe9a0cbbdc1748688ffedad006042b6f45
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-12-12 15:56:09 +0900

    Divide alphabet to upper and lower Loop through each upper/lower case

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,34 +1,44 @@
-def caesar_cipher(input_string = '', key = 0)
-  lowercase_string = input_string.downcase.split('')
-  # [‘z’, ‘a’, ‘b’, ‘c’]
+require 'pry-byebug'
 
-  alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  alphabet_array = alphabet.split('')
+def caesar_cipher(input_string = '', key = 0)
+  string = input_string.split('')
+
+  binding.pry
+
+  alphabet_lower = ' abcdefghijklmnopqrstuvwxyz'
+  alphabet_upper = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
   alphabet_indices = []
   shifted_string = []
 
-  lowercase_string.each do |character|
-    alphabet_array.each_with_index do |alphabet_char, index|
+  string.each do |character|
+    alphabet_lower.each_with_index do |alphabet_char, index|
+      if character == alphabet_char
+        alphabet_indices.push(index)
+        break
+      end
+    end
+    alphabet_upper.each_with_index do |alphabet_char, index|
       if character == alphabet_char
         alphabet_indices.push(index)
         break
       end
     end
   end
-  # [25, 0, 1, 2]
+
+  binding.pry
 
   shifted_indices = alphabet_indices.map do |index|
-    (index + key) % alphabet.length
+    (index + key) % alphabet_lower.length
   end
-  # [1, 2, 3, 4]
+
+  binding.pry
 
   shifted_indices.each do |index|
     shifted_string.push(alphabet_array[index])
   end
-  # [‘b’, ‘c’, ‘d, ‘e’]
 
   p shifted_string.join
 end
 
-caesar_cipher('zabc', 2)
+caesar_cipher('Zabc m', 2)
