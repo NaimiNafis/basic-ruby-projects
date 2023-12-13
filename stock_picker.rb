0ce96e0c98a8cc9f6b368d5565ef1f1b9f0319d0
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-12-13 12:59:11 +0900

    Updat min_day variable

--- stock_picker.rb
+++ stock_picker.rb
@@ -5,16 +5,18 @@ def stock_picker(array)
   buy_day = 0
   sell_day = 0
   max_profit = 0
-  min_price = array[0]
+  min_price = array.first
+  min_day = 0
 
   array.each_with_index do | current_price , today |
-    if min_price > current_price && sell_day > today
+    if min_price > current_price
       min_price = current_price
-      buy_day = today
+      min_day = today
     end
     profit = current_price - min_price
     if profit > max_profit
       max_profit = profit
+      buy_day = min_day
       sell_day = today
     end
   end
